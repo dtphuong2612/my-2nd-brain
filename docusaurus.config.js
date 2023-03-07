8c3c124060aa446028f41df4acdccbb3614d1d8d
dtphuong2612 <duongthithuphuong26122001@gmail.com>
2023-03-07 14:06:11 +0700

    upate

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,16 +22,12 @@ const config = {
   projectName: 'my-2rd-brain', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
 
   presets: [
     [
