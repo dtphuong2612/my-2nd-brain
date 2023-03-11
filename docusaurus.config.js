34dd622329e44a9440a26641a79e8c553eccb9e3
dtphuong2612 <duongthithuphuong26122001@gmail.com>
2023-03-11 23:17:28 +0700

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,12 +37,23 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
+        stylesheets: [
+          {
+            href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+            type: 'text/css',
+            integrity:
+              'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+            crossorigin: 'anonymous',
+          },
+        ],
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
