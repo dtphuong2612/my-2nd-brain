acdb97b8eeb8b9816f57497fd9c32cae43b418ba
dtphuong2612 <duongthithuphuong26122001@gmail.com>
2023-03-11 23:29:59 +0700

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,8 +3,7 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const math = require('remark-math');
-const katex = require('rehype-katex');
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My 2rd Brain',
@@ -37,22 +36,12 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          remarkPlugins: [math],
-          rehypePlugins: [katex],
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          stylesheets: [
-              {
-                href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-                type: 'text/css',
-                integrity:
-                  'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-                crossorigin: 'anonymous',
-              },
-            ]
+
         },
         blog: {
           showReadingTime: true,
